55886dc3a9ee1c1289242b33c174948bd1a0eddb
Kevin Tu <kevin.xander.tu@gmail.com>
2024-05-21 20:19:48 -0500

    Add TileCollision

---
+++ include/TileCollision.hpp
@@ -0,0 +1,50 @@
+/**
+ * @file TileCollision.hpp
+ * @author Grayedsol (grayedsol@gmail.com)
+ * @brief @copybrief TileCollision
+ * @copyright Copyright (c) 2024
+ */
+#pragma once
+#include "Tile.hpp"
+#include "FileResource.hpp"
+#include "SparseSet.hpp"
+
+/**
+ * @brief Holds collision data for a tileset.
+ * 
+ */
+struct TileCollision : public FileResource {
+    using TileId = Tile::TileId;
+    
+    static const TileId MAX_COLLISIONS = 256;
+
+    SparseSet<SDL_FRect, TileId, MAX_COLLISIONS> collisions;
+
+    bool loaded = false;
+
+    TileCollision(const char* path) : FileResource(path) {}
+
+    ~TileCollision() {}
+
+    TileCollision(const TileCollision&) = delete;
+    TileCollision& operator=(const TileCollision&) = delete;
+
+    friend void swap(TileCollision& lhs, TileCollision& rhs) {
+        using std::swap;
+	    swap(static_cast<FileResource&>(lhs), static_cast<FileResource&>(rhs));
+        swap(lhs.collisions, rhs.collisions);
+        swap(lhs.loaded, rhs.loaded);
+    }
+
+    TileCollision(TileCollision&& other) { swap(*this, other); }
+
+    bool load(GRY_Game* game) final override;
+
+    /**
+     * @brief Get the collision rect of a tile.
+     * 
+     * @param tile Tile to get collision rect of.
+     * @return The collision rect if the tile has collision, otherwise nullptr.
+     */
+    const SDL_FRect* getCollision(TileId tile);
+};
---
+++ src/TileCollision.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file TileCollision.cpp
+ * @author Grayedsol (grayedsol@gmail.com)
+ * @copyright Copyright (c) 2024
+ */
+#include "TileCollision.hpp"
+#include "GRY_Tiled.hpp"
+
+bool TileCollision::load(GRY_Game* game) {
+    if (loaded) { return true; }
+
+    /* Open the tileset file */
+    GRY_JSON::Document tilesetDoc;
+    GRY_JSON::loadDoc(tilesetDoc, path);
+
+    /* Load in collision rectangles */
+    for (auto& tile : tilesetDoc["tiles"].GetArray()) {
+        TileId id = tile["id"].GetUint() + 1; /* Add 1 because it's 1-based indexing */
+
+        if (!tile.HasMember("objectgroup")) { continue; }
+
+        const GRY_JSON::Value& objects = tile["objectgroup"]["objects"];
+
+        /* Ensure there is a rectangle */
+        assert(objects.GetArray().Size() > 0);
+        SDL_FRect collisionRect = GRY_Tiled::getRect(objects.GetArray()[0]);
+
+        /* Add data to sparse set */
+        collisions.add(id, collisionRect);
+    }
+
+    loaded = true;
+    return false;
+}
+
+const SDL_FRect* TileCollision::getCollision(TileId tile) {
+    if (!collisions.contains(tile)) { return nullptr; }
+    return &collisions.get(tile);
+}
