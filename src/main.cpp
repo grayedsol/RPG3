d43449ff7f7181b4ead8460800ec51763ab1f3a9
Kevin Tu <kevin.xander.tu@gmail.com>
2024-06-01 22:37:10 -0500

    Correct naming of imagePath to scenePath

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int WINDOW_HEIGHT = 540;
 int TARGET_FPS = 120;
 bool USE_VSYNC = true;
 
-const char* imagePath = "assets/tilesetscene/scene.json";
+const char* scenePath = "assets/tilesetscene/scene.json";
 
 #ifdef _WIN32
 #define NOMINMAX
@@ -28,7 +28,7 @@ _Use_decl_annotations_ int WINAPI WinMain(
     GRY_Game* grayGame = new GRY_Game(WINDOW_WIDTH, WINDOW_HEIGHT, TARGET_FPS, USE_VSYNC);
 	
 	/* Add a scene */
-	grayGame->stackScene(new TilesetScene(grayGame, imagePath));
+	grayGame->stackScene(new TilesetScene(grayGame, scenePath));
 
 	grayGame->runGame();
 
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
 	GRY_Game* grayGame = new GRY_Game(WINDOW_WIDTH, WINDOW_HEIGHT, TARGET_FPS, USE_VSYNC);
 	
 	/* Add a scene */
-	grayGame->stackScene(new TilesetScene(grayGame, imagePath));
+	grayGame->stackScene(new TilesetScene(grayGame, scenePath));
 
 	grayGame->runGame();
 
