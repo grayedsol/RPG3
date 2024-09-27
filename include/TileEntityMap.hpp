c8384f4f7db6e394dc204f4021a52f2e3adfc592
Kevin Tu <kevin.xander.tu@gmail.com>
2024-09-26 22:23:49 -0500

    Add WIP TileEntityMap

---
+++ include/TileEntityMap.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file TileEntityMap.hpp
+ * @author Grayedsol (grayedsol@gmail.com)
+ * @copybrief TileEntityMap
+ * @copyright Copyright (c) 2024
+ */
+#pragma once
+#include "Tileset.hpp"
+#include "GRY_ECS.hpp"
+
+/**
+ * @brief Represents the entities of a tile map.
+ * 
+ */
+struct TileEntityMap : public FileResource {
+	using entity = ECS::entity;
+	using EntityLayer = std::vector<entity>;
+
+	std::vector<EntityLayer> entityLayers;
+
+	std::vector<Tileset> tilesets;
+
+	GRY_ECS<int> ecs;
+
+	TileEntityMap(const char* path) : FileResource(path) {}
+
+	~TileEntityMap() = default;
+
+	TileEntityMap(const TileEntityMap&) = delete;
+	TileEntityMap& operator=(const TileEntityMap&) = delete;
+
+	friend void swap(TileEntityMap& lhs, TileEntityMap& rhs) {
+		using std::swap;
+		swap(static_cast<FileResource&>(lhs), static_cast<FileResource&>(rhs));
+		swap(lhs.entityLayers, rhs.entityLayers);
+		swap(lhs.tilesets, rhs.tilesets);
+	}
+
+	TileEntityMap(TileEntityMap&& other) noexcept { swap(*this, other); }
+
+	bool load(GRY_Game* game) final override;
+};
---
+++ src/TileEntityMap.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file TileEntityMap.cpp
+ * @author Grayedsol (grayedsol@gmail.com) 
+ * @copyright Copyright (c) 2024
+ */
+#include "TileEntityMap.hpp"
+
+bool TileEntityMap::load(GRY_Game *game) {
+	return false;
+}
