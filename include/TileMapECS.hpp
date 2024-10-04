3a86d19cdafe4befd7549e8b91cc1f8ff3a52414
Kevin Tu <kevin.xander.tu@gmail.com>
2024-10-04 15:56:28 -0500

    Add TileMapECS

---
+++ include/TileMapECS.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file TileMapEntityComponentsList.hpp
+ * @author Grayedsol (grayedsol@gmail.com)
+ * @brief Defines components needed for an ECS in a TileMap scene.
+ * @copyright Copyright (c) 2024
+ */
+#pragma once
+#include "GRY_ECS.hpp"
+#include "Components.hpp"
+#include "TileComponents.hpp"
+
+/**
+ * @copybrief GRY_ECS
+ * 
+ */
+using TileMapECS = GRY_ECS<
+	Position2,
+	Velocity2,
+	Actor,
+	ActorTexture
+>;
