ccc60223447e0698a373d64444bbac1f7a85bed1
Kevin Tu <kevin.xander.tu@gmail.com>
2025-01-15 14:13:32 -0600

    Change external/imgui.cmake to FetchContent Using FetchContent populates at configure time, unlike ExternalProject.

--- external/imgui.cmake
+++ external/imgui.cmake
@@ -1,17 +1,15 @@
 # Download imgui
-ExternalProject_Add(
+FetchContent_Declare(
     imgui
     PREFIX "external/imgui"
     GIT_REPOSITORY "https://github.com/ocornut/imgui.git"
     GIT_TAG master
     TIMEOUT 10
-    CMAKE_ARGS
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
 )
 
-ExternalProject_Get_Property(imgui SOURCE_DIR)
-set(IMGUI_SOURCE_DIR ${SOURCE_DIR})
+FetchContent_MakeAvailable(imgui)
+
+FetchContent_GetProperties(imgui
+	SOURCE_DIR IMGUI_SOURCE_DIR
+)
 set(IMGUI_INCLUDE_DIR ${SOURCE_DIR}/include)
