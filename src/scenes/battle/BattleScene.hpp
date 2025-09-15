a0e0a2e118d1cd588ef486e9203e9dd0cbd2957f
Kevin Tu <kevin.xander.tu@gmail.com>
2025-09-14 20:18:05 -0500

    Add character/page variables

--- src/scenes/battle/BattleScene.hpp
+++ src/scenes/battle/BattleScene.hpp
@@ -7,11 +7,15 @@
 #pragma once
 #include "Scene.hpp"
 #include "SoundResource.hpp"
+#include <stdint.h>
 
 class GRY_PixelGame;
 
 class BattleScene : public Scene {
 private:
+	uint8_t currentCharacter = 0;
+	uint8_t currentPage = 0;
+
 	void setControls() final;
 public:
 	BattleScene(GRY_PixelGame* pGame, const char* scenePath);
@@ -31,4 +35,8 @@ public:
 	 * @copydoc Scene::load
 	 */
 	bool load() final;
+
+	uint8_t getCurrentCharacter() { return currentCharacter; }
+
+	uint8_t getCurrentPage() { return currentPage; }
 };
