b40a3c0e18fc8458081dca39a875425f88f2b1be
Kevin Tu <kevin.xander.tu@gmail.com>
2025-11-22 17:17:25 -0600

    BattleScene plans

--- src/scenes/battle/BattleScene.cpp
+++ src/scenes/battle/BattleScene.cpp
@@ -7,7 +7,104 @@ void BattleScene::setControls() {
 	controls.mapCmd(GCmd::BattleRight, VirtualButton::GAME_UP);
 }
 
-BattleScene::BattleScene(GRY_PixelGame* pGame, const char* scenePath) : Scene((GRY_Game*)pGame, scenePath) {
+void BattleScene::processInput() {
+	GCmd cmd = readSingleInput();
+
+	DirectionId direction = DirectionId::DirectionIdSize;
+	switch (cmd) {
+	case GCmd::BattleDown:
+		direction = DirectionId::Down;
+		break;
+	case GCmd::BattleUp:
+		direction = DirectionId::Up;
+		break;
+	case GCmd::BattleLeft:
+		direction = DirectionId::Left;
+		break;
+	case GCmd::BattleRight:
+		direction = DirectionId::Right;
+		break;
+	default:
+		break;
+	}
+
+	if (direction == DirectionId::DirectionIdSize) { return; }
+
+	InputId input = static_cast<InputId>((currentPage << 2) + direction);
+	GRY_Assert(input < InputIdSize, "Invalid battle input state.");
+
+	switch (input) {
+	case InputId::Flee:
+		flee();
+		break;
+	case InputId::Attack:
+		attack();
+		break;
+	case InputId::Item:
+		goToItemPage();
+		break;
+	case InputId::Skill:
+		goToSkillPage();
+		break;
+	case InputId::ItemDown:
+		itemPageDown();
+		break;
+	case InputId::ItemUp:
+		itemPageUp();
+		break;
+	case InputId::ItemToMain:
+	case InputId::SkillToMain:
+		goToMainPage();
+		break;
+	case InputId::Skill3:
+		useSkill3();
+		break;
+	case InputId::Skill1:
+		useSkill1();
+		break;
+	case InputId::Skill2:
+		useSkill2();
+		break;
+	default:
+		break;
+	}
+}
+
+void BattleScene::flee() {
+}
+
+void BattleScene::attack() {
+}
+
+void BattleScene::goToItemPage() {
+}
+
+void BattleScene::goToSkillPage() {
+}
+
+void BattleScene::goToMainPage() {
+}
+
+void BattleScene::itemPageUp() {
+}
+
+void BattleScene::itemPageDown() {
+}
+
+void BattleScene::itemPageSelect() {
+}
+
+void BattleScene::useSkill1() {
+}
+
+void BattleScene::useSkill2() {
+}
+
+void BattleScene::useSkill3() {
+}
+
+BattleScene::BattleScene(GRY_PixelGame *pGame, const char *scenePath, BattleSceneInfo sceneInfo) : Scene((GRY_Game *)pGame, scenePath),
+																								   sceneInfo(sceneInfo) {
 }
 
 void BattleScene::init() {}
--- src/scenes/battle/BattleScene.hpp
+++ src/scenes/battle/BattleScene.hpp
@@ -11,14 +11,70 @@
 
 class GRY_PixelGame;
 
+struct BattleSceneInfo {
+
+};
+
 class BattleScene : public Scene {
 private:
+	BattleSceneInfo sceneInfo;
+
+	enum InputId : uint8_t {
+		Flee = 0,
+		Attack = 1,
+		Item = 2,
+		Skill = 3,
+		ItemDown = 4,
+		ItemUp = 5,
+		ItemSelect = 6,
+		ItemToMain = 7,
+		Skill3 = 8,
+		Skill1 = 9,
+		SkillToMain = 10,
+		Skill2 = 11,
+		InputIdSize = 12
+	};
+
+	enum PageId : uint8_t {
+		MainPage = 0,
+		ItemPage = 1,
+		SkillPage = 2
+	};
+
+	enum DirectionId : uint8_t {
+		Down = 0,
+		Up = 1,
+		Left = 2,
+		Right = 3,
+		DirectionIdSize = 4
+	};
+
 	uint8_t currentCharacter = 0;
 	uint8_t currentPage = 0;
 
 	void setControls() final;
+
+	void processInput();
+
+	void flee();
+
+	void attack();
+	
+	void goToItemPage();
+
+	void goToSkillPage();
+
+	void goToMainPage();
+
+	void itemPageUp();
+	void itemPageDown();
+	void itemPageSelect();
+	
+	void useSkill1();
+	void useSkill2();
+	void useSkill3();
 public:
-	BattleScene(GRY_PixelGame* pGame, const char* scenePath);
+	BattleScene(GRY_PixelGame* pGame, const char* scenePath, BattleSceneInfo sceneInfo);
 
 	/**
 	 * @copydoc Scene::init
