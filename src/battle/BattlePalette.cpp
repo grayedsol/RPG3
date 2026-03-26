99e76ed32456317291b5ddf0ce57dca40ff7cbe5
Kevin Tu <kevin.xander.tu@gmail.com>
2026-03-25 21:27:11 -0700

    Implement switchFighters function

--- src/battle/BattlePalette.cpp
+++ src/battle/BattlePalette.cpp
@@ -76,4 +76,16 @@ void Battle::Palette::itemPageUp() {}
 
 void Battle::Palette::itemPageDown() {}
 
-void Battle::Palette::switchFighters() {}
+void Battle::Palette::switchFighters() {
+	ActorFlags* flags = scene->getActors().flags;
+	FighterId nextFighter = static_cast<FighterId>(currentFighter + 1);
+	for (uint8_t i = 1; i < FighterId::MaxFighters; i++) {
+		if (nextFighter == FighterId::MaxFighters) { nextFighter = FighterId::Fighter0; }
+		if (flags[nextFighter] & (1 << ActorFlag::ACTOR_IDLE)) {
+			currentFighter = nextFighter;
+			return;
+		}
+		nextFighter = static_cast<FighterId>(nextFighter + 1);
+	}
+	currentFighter = FighterId::MaxFighters;
+}
